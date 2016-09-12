39874c0b426f8bf3b2f6f4306376b8a026ff4550
Xiang Wang <ubuntu@mac.cn>
2016-09-12 21:25:32 +0800

    week1_1

--- week1_1.py
+++ week1_1.py
@@ -12,7 +12,7 @@ class Job(object):
     def __init__(self, weight, length):
         self.weight = weight
         self.length = length
-        self.diff = weight-length
+        self.sort = length-weight
 
     @classmethod
     def create_from_line(cls, line):
@@ -22,27 +22,28 @@ class Job(object):
 
 class Diff(object):
     """ 每个diff一致的Jobs的集合,不能存在2个 diff 的 self.diff 一致 """
-    def __init__(self, diff):
-        self.diff = diff
-        self.data = []  # 里面存的是每个 jobs 的 weight 的相反数
+    def __init__(self, sort):
+        self.sort = sort
+        self.index = []  # 所有的-weight的排序
+        self.data = {}  # 
 
     def insert(self, job):
-        heapq.heappush(self.data, -job.weight)
+        if -job.weight not in self.data:
+            self.data[-job.weight] = []
+            heapq.heappush(self.index, -job.weight)
+        self.data[-job.weight].append(job)
 
 
 class Diff_heap(object):
     """ 所有的 diff 构成的集合 """
 
     def __init__(self):
-        self.index = []  # [0,1,2,3,4,5] 所有weight-length的集合
-        self.index_dict = {}  # 快速判断某个diff是否属于diff_heap,其实和下面的重复了
+        self.index = []  # [0,1,2,3,4,5] 所有job.sort的集合
         self.data = {}  # diff 以 diff.diff 为Key, self为value的dict
-        pass
 
     def insert(self, diff):
-        heapq.heappush(self.index, diff.diff)
-        self.data[diff.diff] = diff
-        self.index_dict[diff.diff] = True
+        heapq.heappush(self.index, diff.sort)
+        self.data[diff.sort] = diff
 
 
 file_obj = open(file_path, 'r')
@@ -51,20 +52,21 @@ diff_heap = Diff_heap()
 
 for i in range(line_number):
     job = Job.create_from_line(file_obj.readline())
-    if job.diff not in diff_heap.index_dict:
-        diff = Diff(job.diff)
+    if job.sort not in diff_heap.data:
+        diff = Diff(job.sort)
         diff.insert(job)
         diff_heap.insert(diff)
     else:
-        diff_heap.data[job.diff].insert(job)
+        diff_heap.data[job.sort].insert(job)
 
 duration = 0  # 随着job的完成，这个duration会一直增加
 cost = 0
 while diff_heap.index:
-    diff_number = heapq.heappop(diff_heap.index)
-    diff = diff_heap.data[diff_number]
-    while diff.data:
-        weight = heapq.heappop(diff.data)
-        duration += -weight - diff_number
-        cost += -weight*duration
+    sort = heapq.heappop(diff_heap.index)
+    diff = diff_heap.data[sort]
+    while diff.index:
+        weight = -heapq.heappop(diff.index)
+        for job in diff.data[-weight]:
+            duration += job.length
+            cost += duration* job.weight
 print(cost)
