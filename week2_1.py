1906c290ee95d70f60ad5c8ae12b0e1509611824
Xiang Wang <ubuntu@mac.cn>
2016-11-09 21:15:19 +0800

    第一题

--- week2_1.py
+++ week2_1.py
@@ -2,24 +2,42 @@
 # -*- coding: utf-8 -*-
 # Xiang Wang @ 2016-11-05 17:09:33
 
+import heapq
+
 
 class Node(object):
-    """ 节点对象 """
+    " number 从 0 到 499 "
 
     def __init__(self, number):
         self.number = number
         self.edges = []
         self.parent = None
+        self.depth = 0
+
+    @property
+    def ancestor(self):
+        if self.parent is None:
+            return self
+        else:
+            return self.parent.ancestor
 
 
 class Edge(object):
-    """ 链接对象 """
 
     def __init__(self, number, node1, node2, weight):
         self.number = number  # 链接的id
         self.node1 = node1
         self.node2 = node2
         self.weight = weight  # 链接的长度
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def __gt__(self, other):
+        return self.weight > other.weight
+
+    def __eq__(self, other):
+        return self.weight == other.weight
 
 
 class Clustering(object):
@@ -30,5 +48,48 @@ class Clustering(object):
         self.nodes = []
         self.edges = []
         self.edge_cnt = 0
+        self.block = self.node_cnt
+        for node_number in range(self.node_cnt):
+            node = Node(number=node_number)
+            self.nodes.append(node)
+        edge_number = 0
         for line in self.input.readlines():
-            node_cnt = int(
+            node1_number, node2_number, edge_length = map(int, line.split(' '))
+            node1 = self.nodes[node1_number-1]
+            node2 = self.nodes[node2_number-1]
+            edge = Edge(number=edge_number, node1=node1, node2=node2, weight=edge_length)
+            node1.edges.append(edge)
+            node2.edges.append(edge)
+            heapq.heappush(self.edges, edge)
+
+    def get_shortest_edge(self):
+        edge = heapq.heappop(self.edges)
+        return edge
+
+    def merge(self):
+        """ 合并最靠近的2个block """
+        edge = self.get_shortest_edge()
+        if edge.node1.ancestor == edge.node2.ancestor:
+            self.merge()
+        else:
+            if edge.node1.ancestor.depth > edge.node2.ancestor.depth:
+                edge.node2.ancestor.parent = edge.node1.ancestor
+            else:
+                edge.node1.ancestor.parent = edge.node2.ancestor
+        self.block -= 1
+
+    def get_space(self):
+        """ 获取当前最靠近的block的距离 """
+        edge = self.edges[0]
+        if edge.node1.ancestor == edge.node2.ancestor:
+            heapq.heappop(self.edges)
+            return self.get_space()
+        else:
+            return edge.weight
+
+
+if __name__ == '__main__':
+    answer = Clustering()
+    while answer.block > 4:
+        answer.merge()
+    print(answer.get_space())
